093b798724eae092dd745aaf3c229bd640de3fb5
kittisakpanlek01 <kittisakpanlek01@gmail.com>
2025-08-29 17:18:47 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -19,21 +19,43 @@ if input_mode == "อัพโหลดไฟล์ Excel":
         st.write("Uploaded Data:")
         st.dataframe(input_df)
         required_cols = ['TEMTAR','ActWidthIn','RMEXTW','PSSPOS_More','PESPOSIn','PESPOSOut','INDH']
+        st.write("Columns in scaler:", scaler.feature_names_in_)
+        st.write("Columns in uploaded file:", input_df.columns.tolist())
 
         # ตรวจว่ามีครบไหม
         if not set(required_cols).issubset(input_df.columns):
-            st.error(f"ไฟล์ Excel ต้องมี columns: {required_cols}")
+            st.error(f"Excel file must contain columns: {list(required_cols)}")
         else:
-            input_df = input_df[required_cols]  # จัดเรียง column ให้ตรงกับ scaler
+            input_df = input_df[list(required_cols)]  # จัดเรียงให้ตรงกับ scaler
             input_data_scaled = scaler.transform(input_df)
             predictions_scaled = model.predict(input_data_scaled)
             predictions = scaler_y.inverse_transform(predictions_scaled)
             input_df['Predicted Width'] = predictions
-            st.write("Predictions:")
             st.dataframe(input_df)
-
     else:
         st.write("กรุณาอัพโหลดไฟล์ Excel เพื่อทำการทำนาย")
+
+    # uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx"])
+    # if uploaded_file is not None:
+    #     input_df = pd.read_excel(uploaded_file)
+    #     st.write("Uploaded Data:")
+    #     st.dataframe(input_df)
+    #     required_cols = ['TEMTAR','ActWidthIn','RMEXTW','PSSPOS_More','PESPOSIn','PESPOSOut','INDH']
+
+    #     # ตรวจว่ามีครบไหม
+    #     if not set(required_cols).issubset(input_df.columns):
+    #         st.error(f"ไฟล์ Excel ต้องมี columns: {required_cols}")
+    #     else:
+    #         input_df = input_df[required_cols]  # จัดเรียง column ให้ตรงกับ scaler
+    #         input_data_scaled = scaler.transform(input_df)
+    #         predictions_scaled = model.predict(input_data_scaled)
+    #         predictions = scaler_y.inverse_transform(predictions_scaled)
+    #         input_df['Predicted Width'] = predictions
+    #         st.write("Predictions:")
+    #         st.dataframe(input_df)
+
+    # else:
+    #     st.write("กรุณาอัพโหลดไฟล์ Excel เพื่อทำการทำนาย")
 else:
     st.write("กรุณากรอกข้อมูลด้านล่าง:")
     # Input fields for features
